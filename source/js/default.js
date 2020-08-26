5452a1deaaf4d574e5a624f0aa8a7ae6bf58c21f
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-08-26 15:31:03 +0300

    Added standar bs3 class for full page

--- source/js/default.js
+++ source/js/default.js
@@ -1,12 +1,6 @@
 'use strict'
 
-$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
-
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
-  });
-});
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+})
